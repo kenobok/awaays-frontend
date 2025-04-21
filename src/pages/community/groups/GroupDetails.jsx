46f5e9e157bb536387844533f98adae21a4ccb16
kenobok <okenneth009@gmail.com>
2025-04-21 08:34:20 +0100

    still on group

--- src/pages/community/groups/GroupDetails.jsx
+++ src/pages/community/groups/GroupDetails.jsx
@@ -1,13 +1,31 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { groups } from '/src/components/utils/UtilsData';
 import '/src/assets/styles/community.css';
 import userImg from '/src/assets/images/hero2.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const GroupDetails = () => {
     const { slug } = useParams();
+    const membersRef = useRef();
+    const [showMembers, setShowMembers] = useState(false);
+
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    
+    const handleClickOutside = (e) => {
+        if (membersRef.current && !membersRef.current.contains(e.target)) {
+            setShowMembers(false);
+        }
+    };
+
 
     const group = useMemo(() => {
         return groups.find(g => slug === g.slug);
@@ -15,17 +33,25 @@ const GroupDetails = () => {
 
     return (
         <div className='group-details-wrp ml-25 max-[768px]:ml-0 pr-5 max-[768px]:px-4'>
-            <div className='flex justify-between mb-5 border-b border-gray-200'>
-                <h3 className='text-center text-[1.3rem] font-semibold'>{group.name}</h3>
-                <div className='space-x-5 mb-1 min-[768px]:space-x-0'>
-                    <button className='border-2 border-[var(--p-color)] py-[1px] px-3 rounded-full cursor-pointer hover:bg-[var(--p-color)] hover:text-white min-[768px]:hidden'>Members</button>
-                    <button className='border-2 border-[var(--p-color)] py-[1px] px-3 rounded-full cursor-pointer hover:bg-[var(--p-color)] hover:text-white'>Join Group</button>
+            <div className='flex mb-6'>
+                <div className='relative flex-1'>
+                    <h3 className='text-left my-auto text-[1.3rem] font-semibold max-[577px]:text-[1.1rem]'>{group.name}</h3>
+                    <div className='absolute'>
+                        <p className='text-[.95rem] inline-block max-[577px]:block mr-10 text-orange-400'><FontAwesomeIcon icon='user-shield'/> {group.admin}</p>
+                        <p className='text-[.95rem] inline-block max-[577px]:block text-blue-500'><FontAwesomeIcon icon='users'/> {group.members.length}</p>
+                    </div>
+                </div>
+                <div className='flex max-[577px]:flex-col items-center space-x-4 max-[577px]:space-x-0 mb-1 min-[768px]:space-x-0'>
+                    <button className={`border-2 border-[var(--p-color)] py-[1px] px-3 rounded-full cursor-pointer hover:bg-[var(--p-color)] hover:text-white min-[768px]:hidden max-[577px]:text-[.9rem] max-[577px]:mb-2 ${showMembers ? 'border-red-400 text-red-500' : ''}`} onClick={() => {setShowMembers(!showMembers)}}>Members</button>
+                    <button className='border-2 border-[var(--p-color)] py-[1px] px-3 rounded-full cursor-pointer hover:bg-[var(--p-color)] hover:text-white max-[577px]:text-[.9rem]'>Join Group</button>
                 </div>
             </div>
-            <div className='flex justify-between gap-x-7 h-[75vh] overflow-y-auto'>
-                <div className={`flex-1 bg-green-400`}></div>
-                
-                <div className={`w-[18rem] max-[1080px]:w-[17rem] h-full bg-blue-200 pt-2 px-3 max-[768px]:hidden`}>
+            <div className='flex justify-between gap-x-5 h-[75vh] overflow-y-auto'>
+                <div className={`flex-1 border border-gray-300 rounded-xl`}>
+
+                </div>
+                                                                                                                                                                                                                                                                                                                                                                               
+                <div ref={membersRef} className={`members-wrp w-[18rem] max-[1080px]:w-[17rem] h-full bg-blue-200 pt-2 px-3 rounded-xl ${showMembers ? 'show' : ''}`}>
                     <h4 className='text-center font-semibold'>Members</h4>
                     <div className={`w-full h-[70vh] overflow-y-auto`}>
                         {
