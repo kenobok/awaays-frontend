5e0f9122ddb68e83fb9e7ecb34563c0002fc5296
kenobok <okenneth009@gmail.com>
2025-05-17 19:31:44 +0100

    routing

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
+  base: '/',
   plugins: [ react(), tailwindcss() ],
   server : { host: '0.0.0.0', port: 3000 },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 })
+
 
