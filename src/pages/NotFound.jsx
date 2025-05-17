cbfcd232e5083e5facf7b06b54b36aa3857520a3
kenobok <okenneth009@gmail.com>
2025-05-17 02:21:35 +0100

    404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom'
+import img404 from '../../src/assets/images/404.png'
 
 
 const NotFound = () => {
     return (
-        <main>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <main className='h-[100vh] w-full flex flex-col justify-center items-center'>
+            <img src={img404} alt='404 not found' />
+            <h1 className='text-[2rem] fon-semibold -translate-y-8'>Page not found</h1>
+            <div className='flex space-x-3 justify-center items-center'>
+                <Link to='/' className='border-2 border-[var(--p-color)] px-4 py-2 rounded-lg block'>Home</Link>
+                <Link to='/give-item' className='border-2 border-[var(--p-color)] px-4 py-2 rounded-lg block'>Give Item</Link>
+                <Link to='/giveaway-items' className='border-2 border-[var(--p-color)] px-4 py-2 rounded-lg block'>Giveaways</Link>
+            </div>
         </main>
     );
 }
