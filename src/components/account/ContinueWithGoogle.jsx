f5c997b2521d951518c6025211dc497edd5d87c5
kenobok <okenneth009@gmail.com>
2025-05-18 14:24:10 +0100

    google auth

--- src/components/account/ContinueWithGoogle.jsx
+++ src/components/account/ContinueWithGoogle.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function ContinueWithGoogle() {
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         window.google.accounts.id.initialize({
             client_id: "972409687092-ap50aanokaofs65m8to9cn3s147f8vcp.apps.googleusercontent.com",
@@ -11,17 +14,16 @@ export function ContinueWithGoogle() {
     const handleGoogleResponse = async (response) => {
         const idToken = response.credential;
 
-        const res = await fetch("https://api.awaays.com/api/auth/google/", {
+        const res = await fetch("https://api.awaays.com/api/account/auth/google/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            credentials: "include", // for httpOnly cookie
+            credentials: "include",
             body: JSON.stringify({ id_token: idToken }),
         });
 
         if (res.ok) {
-            // Handle success — update auth context or redirect
             console.log("Logged in with Google");
         } else {
             console.error("Google login failed");
@@ -29,15 +31,20 @@ export function ContinueWithGoogle() {
     };
 
     const triggerGoogleLogin = () => {
-        window.google.accounts.id.prompt(); // opens Google's popup
+        setLoading(true);
+        try {
+            window.google.accounts.id.prompt();
+        } catch (error) {
+            console.error("An error occurred, try again...");
+        } finally {
+            setLoading(false);
+        }
     };
-
-    console.log(window.location.origin);
 
     return (
         <button type="button" className="bg-[var(--bg-color)] leading-[0.8rem] rounded-lg hover:bg-white transition flex items-center justify-center" onClick={triggerGoogleLogin}>
             <img src="https://www.svgrepo.com/show/355037/google.svg" alt="Google" className="w-5 h-5 mr-1"/>
-            Continue with Google
+            { loading ? <FontAwesomeIcon icon='spinner' className="animate-spin px-5 text-[1rem]" /> : 'Continue with Google' }
         </button>
     );
 }
